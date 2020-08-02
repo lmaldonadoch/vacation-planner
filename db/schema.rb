8ff534fbbab20797ff5fbcfae8c8c7c944f74523
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-08-02 17:09:35 -0500

    Changed seed file to comply with requested design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_172647) do
+ActiveRecord::Schema.define(version: 2020_08_02_213738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_172647) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "city"
     t.index ["destination_id"], name: "index_images_on_destination_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,38 +12,38 @@ u2 = User.create(name: 'Luis', email: 'luis@vacationplanner.com', password: '123
 
 u3 = User.create(name: 'Carlos', email: 'carlos@vacationplanner.com', password: '123456', password_confirmation: '123456')
 
-d1 = Destination.create(place: 'Tulum, Q. Ro, Mexico')
+d1 = Destination.create(place: 'Mexico')
 
-d2 = Destination.create(place: 'Bacalar, Q. Ro, Mexico')
+d2 = Destination.create(place: 'Peru')
 
-d3 = Destination.create(place: 'Cusco, Peru')
+d3 = Destination.create(place: 'Canada')
 
-d4 = Destination.create(place: 'London, Ontario, Canada')
+d4 = Destination.create(place: 'Brazil')
 
-d5 = Destination.create(place: 'Queretaro, Mexico')
+d5 = Destination.create(place: 'Greece')
 
-d6 = Destination.create(place: 'Santa Maria, RS, Brazil')
+d6 = Destination.create(place: 'Japan')
 
-it1 = Image.create(destination_id: 1, image_url: 'https://images.unsplash.com/photo-1568548655200-b91935d692ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
-it2 = Image.create(destination_id: 1, image_url: 'https://images.unsplash.com/photo-1568548740292-7ccd65802bb6?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80')
-it3 = Image.create(destination_id: 1, image_url: 'https://images.unsplash.com/photo-1561464380-c01da572f971?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=830&q=80')
+im1 = Image.create(destination_id: 1, city: 'Tulum', image_url: 'https://images.unsplash.com/photo-1568548655200-b91935d692ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+im2 = Image.create(destination_id: 1, city: 'Bacalar', image_url: 'https://images.unsplash.com/photo-1576246837439-dd0d7f347759?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80')
+im3 = Image.create(destination_id: 1,city: 'Queretaro', image_url: 'https://images.unsplash.com/photo-1590125664444-1d0404bc8adc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')
 
-ib1 = Image.create(destination_id: 2, image_url: 'https://images.unsplash.com/photo-1576246837439-dd0d7f347759?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80')
-ib2 = Image.create(destination_id: 2, image_url: 'https://images.unsplash.com/photo-1529671241112-2c70ebef934d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=848&q=80')
-ib3 = Image.create(destination_id: 2, image_url: 'https://images.unsplash.com/photo-1594072551423-01ec561af5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1533&q=80')
+ip1 = Image.create(destination_id: 2, city: 'Cusco (Machu Picchu', image_url: 'https://images.unsplash.com/photo-1533856797653-6f6dbf370efc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+ip2 = Image.create(destination_id: 2, city: 'Cusco (Seven Colors Mountain)', image_url: 'https://images.unsplash.com/photo-1545330785-15356daae141?ixlib=rb-1.2.1&auto=format&fit=crop&w=854&q=80')
+ip3 = Image.create(destination_id: 2, city: 'Lima', image_url: 'https://images.unsplash.com/photo-1531968455001-5c5272a41129?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1381&q=80')
 
-ic1 = Image.create(destination_id: 3, image_url: 'https://images.unsplash.com/photo-1533856797653-6f6dbf370efc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
-ic2 = Image.create(destination_id: 3, image_url: 'https://images.unsplash.com/photo-1545330785-15356daae141?ixlib=rb-1.2.1&auto=format&fit=crop&w=854&q=80')
-ic3 = Image.create(destination_id: 3, image_url: 'https://images.unsplash.com/photo-1578594903885-51d6d8bcdbb7?ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80')
+ic1 = Image.create(destination_id: 3, city: 'Montreal', image_url: 'https://images.unsplash.com/photo-1558567612-559ce068ba31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+ic2 = Image.create(destination_id: 3, city: 'London', image_url: 'https://images.unsplash.com/photo-1595660192522-27fb00defb4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=966&q=80')
+ic3 = Image.create(destination_id: 3, city: 'Québec City', image_url: 'https://images.unsplash.com/photo-1558486517-1025f6d30fc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
 
-il1 = Image.create(destination_id: 4, image_url: 'https://images.unsplash.com/photo-1595660192522-27fb00defb4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=966&q=80')
-il2 = Image.create(destination_id: 4, image_url: 'https://images.unsplash.com/photo-1523312628884-26d58dc8bd61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1034&q=80')
-il3 = Image.create(destination_id: 4, image_url: 'https://images.unsplash.com/photo-1500185760083-2355a0bc5d35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+ib1 = Image.create(destination_id: 4, city: 'Rio de Janeiro', image_url: 'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')
+ib2 = Image.create(destination_id: 4, city: 'São Paulo', image_url: 'https://images.unsplash.com/photo-1524171877404-222de92181e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')
+ib3 = Image.create(destination_id: 4, city: 'Ponta Negra', image_url: 'https://images.unsplash.com/photo-1515898698999-18f625d67499?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')
 
-iq1 = Image.create(destination_id: 5, image_url: 'https://images.unsplash.com/photo-1590125664444-1d0404bc8adc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
-iq2 = Image.create(destination_id: 5, image_url: 'https://images.unsplash.com/photo-1548402539-86c2670fe771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80')
-iq3 = Image.create(destination_id: 5, image_url: 'https://images.unsplash.com/photo-1512442827816-8e5a088619c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1019&q=80')
+ig1 = Image.create(destination_id: 5, city: 'Athens', image_url: 'https://images.unsplash.com/photo-1531816458010-fb7685eecbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')
+ig2 = Image.create(destination_id: 5, city: 'Santorini', image_url: 'https://images.unsplash.com/photo-1560703650-ef3e0f254ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')
+ig3 = Image.create(destination_id: 5, city: 'Kerkira', image_url: 'https://images.unsplash.com/photo-1500198894528-3819de6ad916?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')
 
-is1 = Image.create(destination_id: 6, image_url: 'https://www.sistur.rs.gov.br/multimidia_promocional/MD_2014021411083211._santuario_de_schoenstatt.jpg')
-is2 = Image.create(destination_id: 6, image_url: 'https://www.sistur.rs.gov.br/multimidia_promocional/MD_20140214104108rg.jpg')
-is3 = Image.create(destination_id: 6, image_url: 'https://www.sistur.rs.gov.br/multimidia_promocional/GD_201401140953452_santa_maria..jpg')
+ij1 = Image.create(destination_id: 6, city: 'Fujiyoshida-shi', image_url: 'https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80')
+ij2 = Image.create(destination_id: 6, city: 'Kyoto', image_url: 'https://images.unsplash.com/photo-1504198453319-5ce911bafcde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1405&q=80')
+ij3 = Image.create(destination_id: 6, city: 'Tokio', image_url: 'https://images.unsplash.com/photo-1570695171845-38ff5b155898?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')
