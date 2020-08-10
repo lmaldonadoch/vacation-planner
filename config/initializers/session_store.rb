f89d96819c624eee755123b38849f2bc0112a395
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-08-10 16:11:27 -0500

    debugging production WIP

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'rr-authentication.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://vacation-planner-ror-react.herokuapp.com/'
  else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
  end
