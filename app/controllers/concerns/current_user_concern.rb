febdb41458465998026667254efff3749da22e32
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-08-10 14:39:11 -0500

    debugging production WIP

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,6 +6,18 @@ module CurrentUserConcern
   end
 
   def set_current_user
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
+    p 'SET CURRENT USER IN CONCERN'
+    p session[:user_id]
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
+    p '*' * 20
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
